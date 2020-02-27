7da87dd50fb857b0af16c206676951cb7379d7e0
William Gonzales <wgonz94@yahoo.com>
2020-02-26 17:17:35 -0800

    Created a State and Dispatch Context to update data throughout app

---
+++ client/src/loginReducer/user/SDContext.js
@@ -0,0 +1,9 @@
+// Context for anything to do with the User
+
+import { createContext } from 'react';
+
+const StateContext = createContext();
+const DispatchContext = createContext();
+
+export default StateContext;
+export default DispatchContext;
